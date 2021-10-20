934a841ed1a4cc32d157e3832e8bf7da0a5e3f82
Ricardo Martini <ricardo.martini@openfin.co>
2021-10-20 10:04:50 -0400

    added content creation to test

--- win.js
+++ win.js
@@ -1,28 +1,38 @@
 window.onload = () => {
     const button = document.querySelector("#win")
 
-button.addEventListener("click", ()=>{
-    createWindow()
-   
-}      
+    button.addEventListener("click", () => {
+        createWindow()
+
+    }
     )
 }
 
 async function createWindow() {
-     
+
     return await fin.Window.create(winOption);
 }
 
 const winOption = {
-    name:'child',
+    name: 'child',
     defaultWidth: 300,
     defaultHeight: 300,
     url: 'http://localhost:3000/link.html',
     frame: true,
     autoShow: true,
-    permissions: {
-        System: {
-            launchExternalProcess: true
-        }
+    contentCreation: {
+        rules: [
+            {
+                match: ['<all_urls>'],
+                behavior: 'window',
+                options: {
+                    permissions: {
+                        System: {
+                            launchExternalProcess: true
+                        }
+                    }
+                }
+            }
+        ]
     }
 }
